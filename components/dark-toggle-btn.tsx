efd7b1befcf7f75e55e5b770e17aa5ec4750a0a5
“nimoseel” <fromsomini@gmail.com>
2023-04-10 15:14:14 +0900

    Design : 마진 값 수정

--- components/dark-toggle-btn.tsx
+++ components/dark-toggle-btn.tsx
@@ -6,8 +6,8 @@ export default function DarkModeToggleBtn(){
     return (
         <button 
             className="
-                inline-flex items-center 
-                bg-gray-100 border-0 py-1 px-3 rounded text-base mt-4 md:mt-0
+                inline-flex items-center
+                bg-gray-100 border-0 py-1 px-3 rounded text-base
                 focus:outline-none 
                 hover:bg-gray-50
                 hover:text-orange-500
