b6ac4c28c42a0476843319625107544a8890926c
“nimoseel” <fromsomini@gmail.com>
2023-04-10 13:25:00 +0900

    Fix : description 추가

--- components/projects/project-item.tsx
+++ components/projects/project-item.tsx
@@ -34,7 +34,7 @@ export default function ProjectItem({data}: DataType){
 
     const title = data.properties.Name.title[0].plain_text
     const github = data.properties.Github.rich_text[0].plain_text
-    // const description = data.properties.Description.rich_text[0].plain_text
+    const description = data.properties.Description.rich_text[0].plain_text
     const imgSrc = data.cover.file?.url
     const link = data.properties.Link.rich_text[0].plain_text
     const Tags = data.properties.Tags.multi_select
@@ -54,7 +54,7 @@ export default function ProjectItem({data}: DataType){
                 />
                 <div className="p-4 flex flex-col">
                     <h1 className="text-2xl font-bold mb-3">{title}</h1>
-                    {/* <h3 className="mt-4 text-xl">{description}</h3> */}
+                    <h3 className="mt-4 text-xl">{description}</h3>
                     <a href={github} className="mb-1">깃허브 바로가기</a>
                     <a href={link} target="_blank" className="mb-1">배포 링크 바로가기</a>
                     <p className="mb-2">작업 기간 : {period}</p>
