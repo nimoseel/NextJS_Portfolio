a2207490e24ce58e0c8b604f44cdc6125da83045
“nimoseel” <fromsomini@gmail.com>
2023-04-14 16:09:55 +0900

    Design : margin 값 수정

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,10 +8,10 @@ export default function Footer(){
                 <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
                     <Link href="/" legacyBehavior>
                         <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-                            <span className="ml-3 text-xl">소민의 포트폴리오</span>
+                            <span className="text-xl">소민의 포트폴리오</span>
                         </a>
                     </Link>
-                <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2020 Tailblocks —
+                <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0">© 2020 Tailblocks —
                     <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank">@knyttneve</a>
                 </p>
                 </div>
