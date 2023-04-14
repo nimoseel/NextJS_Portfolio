c1870c1b7ee3cf0a15ddf799475003adaf02e637
“nimoseel” <fromsomini@gmail.com>
2023-04-14 13:40:33 +0900

    Bug : children 타입 지정

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,8 +1,9 @@
 // 모든 레이아웃을 집어넣는 개념
+import { ReactNode } from "react";
 import Header from "./header"
 import Footer from "./footer"
 
-export default function Layout({children}){
+export default function Layout({children} : {children: ReactNode}){
     return (
         <div className="bg-primary">
             <Header/>
