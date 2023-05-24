63c68419e67c104e769b07fcaef4887f1026562e
“nimoseel” <fromsomini@gmail.com>
2023-05-24 22:52:18 +0900

    Bug : 타입 수정

--- components/todo/todo-item.tsx
+++ components/todo/todo-item.tsx
@@ -1,9 +1,21 @@
+import { ChangeEvent } from 'react'
+import { useRecoilState } from 'recoil';
+import { todoState } from '../../recoil/TodoAtom'
+
 type TodoItemProps = {
-    id: number;
+    id: string;
     content: string;
 };
 
 export default function TodoItem({id, content}: TodoItemProps){
+    const [ todoList, setTodoList ] = useRecoilState(todoState);
+
+    const onDelete = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        // const newTodoList = todoList.filter((v) => v.id !== e.target.id)
+        // setTodoList(newTodoList)
+        console.log(e.target.id)
+    }
+
     return(
         <li className="list-none border-solid border-2 border-indigo-600 rounded-md mb-3 p-4 flex justify-between items-center" key={id}>
             <p className="pl-1">{content}</p>
